16f02b219963ac9ca86ba090b2f60aa1db1958c9
abhijeet <jadhavabhijeet888@gmail.com>
2021-10-14 17:10:14 +0530

    Initial commit

---
+++ src/Layout/Header.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { Navbar, NavbarBrand} from "reactstrap";
+
+
+const Header = () => {
+  return (
+    <Navbar color="info" light>
+      <NavbarBrand className="text-white">E-Commerce Shop</NavbarBrand>
+    
+    </Navbar>
+  );
+};
+export default Header;
